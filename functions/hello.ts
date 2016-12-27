d0990d9d2f476b215c440cfa9c23c8a70bfa41ad
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2016-12-27 21:37:35 +0900

    add type

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,4 +1,6 @@
-export function hello(event, context, callback): void {
+import {LambdaExecutionContext} from "../types";
+
+export function hello(event, context: LambdaExecutionContext, callback): void {
   const response = {
     statusCode: 200,
 
@@ -6,7 +8,7 @@ export function hello(event, context, callback): void {
     },
 
     body: JSON.stringify({
-      "message": "Hello TypeScript!!!!"
+      message: `Hello!!!! I am a function named "${context.functionName}"`
     })
   };
 
